de2116a7752eb27e003f3438b595054fca655fdd
jackmedrano <jack_medrano@hotmail.com>
2019-05-01 17:48:25 -0300

    flujo de unidad de medida

--- StockCustom/src/main/java/com/svcg/StockCustom/repository/UnidadMedidaRepository.java
+++ StockCustom/src/main/java/com/svcg/StockCustom/repository/UnidadMedidaRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.svcg.StockCustom.entity.UnidadMedida;
 
-@Repository
+@Repository("unidadMedidaRepository")
 public interface UnidadMedidaRepository extends JpaRepository<UnidadMedida, Serializable> {
 
 	 UnidadMedida findByNombre(String nombre);
--- StockCustom/src/main/java/com/svcg/StockCustom/service/impl/UnidadMedidaServiceImpl.java
+++ StockCustom/src/main/java/com/svcg/StockCustom/service/impl/UnidadMedidaServiceImpl.java
@@ -1,12 +1,32 @@
 package com.svcg.StockCustom.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.svcg.StockCustom.component.Messages;
 import com.svcg.StockCustom.entity.UnidadMedida;
+import com.svcg.StockCustom.repository.UnidadMedidaRepository;
 import com.svcg.StockCustom.service.UnidadMedidaService;
 
+@Service("unidadMedidaServiceImpl")
 public class UnidadMedidaServiceImpl implements UnidadMedidaService {
+
+	@Autowired
+	Messages messages;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(UserServiceImpl.class);
+
+	@Autowired
+	@Qualifier("unidadMedidaRepository")
+	private UnidadMedidaRepository unidadMedidaRepository;
 
 	@Override
 	public UnidadMedida saveUnidadMedida(UnidadMedida unidadMedida) {
@@ -22,8 +42,14 @@ public class UnidadMedidaServiceImpl implements UnidadMedidaService {
 
 	@Override
 	public UnidadMedida getUnidadMedidaByNombre(String nombre) {
-		// TODO Auto-generated method stub
-		return null;
+		com.svcg.StockCustom.entity.UnidadMedida unidadMedida = unidadMedidaRepository
+				.findByNombre(nombre);
+		if (unidadMedida == null) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+					this.messages.get("MESSAGE_NOT_FOUND_USUARIO"), null);
+		}
+
+		return unidadMedida;
 	}
 
 	@Override
@@ -36,6 +62,12 @@ public class UnidadMedidaServiceImpl implements UnidadMedidaService {
 	public UnidadMedida updateUnidadMedida(UnidadMedida unidadMedida) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private boolean unidadMedidaNameExist(String nombre) {
+		com.svcg.StockCustom.entity.UnidadMedida unidadMedida = unidadMedidaRepository
+				.findByNombre(nombre);
+		return unidadMedida != null;
 	}
 
 }
